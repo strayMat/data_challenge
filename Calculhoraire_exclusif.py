f13566cfcc0be7bdd2214bb21af4c40c42db10c8
Johnson-Johnson <etienne.bamas@orange.fr>
2016-12-06 11:25:19 +0100

    Add files via upload

--- Calculhoraire_exclusif.py
+++ Calculhoraire_exclusif.py
@@ -14,34 +14,66 @@ def intervalle(X):
    
    
 def centre(X):
-    if(X=="CAT"): return 18
-    if(X=="CMS"): return 0
-    if(X=="Crises"): return 1
-    if(X=="Domicile"): return 2
-    if(X=="Evenements"): return 19
-    if(X=="Gestion"): return 3
-    if(X=="Gestion Amex"): return 20
-    if(X=="Gestion Clients"): return 21
-    if(X=="Gestion DZ"): return 22
-    if(X=="Gestion - Accueil Telephonique"): return 4
-    if(X=="Gestion Assurances"): return 5
-    if(X=="Gestion Relation Clienteles"): return 6
-    if(X=="Gestion Renault"): return 7
-    if(X=="Japon"): return 8
-    if(X=="Manager"): return 23
-    if(X=="Mécanicien"): return 24
-    if(X=="Médical"): return 9
-    if(X=="Nuit"): return 10
-    if(X=="RENAULT"): return 11
-    if(X=="Prestataires"): return 25
-    if(X=="RTC"): return 26
-    if(X=="Regulation Medicale"): return 12
-    if(X=="SAP"): return 13
-    if(X=="Services"): return 14
-    if(X=="Tech. Axa"): return 15
-    if(X=="Tech. Inter"): return 16
-    if(X=="Tech. Total"): return 27
-    if(X=="Téléphonie"): return 17
+    if(X=="CAT"): return 0
+    if(X=="CMS"): return 1
+    if(X=="Crises"): return 2
+    if(X=="Domicile"): return 3
+    if(X=="Evenements"): return 4
+    if(X=="Gestion"): return 5
+    if(X=="Gestion - Accueil Telephonique"): return 6
+    if(X=="Gestion Amex"): return 7
+    if(X=="Gestion Assurances"): return 8
+    if(X=="Gestion Clients"): return 9
+    if(X=="Gestion DZ"): return 10
+    if(X=="Gestion Relation Clienteles"): return 11
+    if(X=="Gestion Renault"): return 12
+    if(X=="Japon"): return 13
+    if(X=="Manager"): return 14
+    if(X=="Mécanicien"): return 15
+    if(X=="Médical"): return 16
+    if(X=="Nuit"): return 17
+    if(X=="Prestataires"): return 18
+    if(X=="Regulation Medicale"): return 19
+    if(X=="RENAULT"): return 20
+    if(X=="RTC"): return 21
+    if(X=="SAP"): return 22    
+    if(X=="Services"): return 23
+    if(X=="Tech. Axa"): return 24
+    if(X=="Tech. Inter"): return 25
+    if(X=="Tech. Total"): return 26
+    if(X=="Téléphonie"): return 27
+    
+    return -1 #autre cas randoms si ça foire
+
+def inverse(X):
+    if(X==0): return "CAT"
+    if(X==1): return "CMS"
+    if(X==2): return "Crises"
+    if(X==3): return "Domicile"
+    if(X==4): return "Evenements"
+    if(X==5): return "Gestion"
+    if(X==6): return "Gestion - Accueil Telephonique"
+    if(X==7): return "Gestion Amex"
+    if(X==8): return "Gestion Assurances"
+    if(X==9): return "Gestion Clients"
+    if(X==10): return "Gestion DZ"
+    if(X==11): return "Gestion Relation Clienteles"
+    if(X==12): return "Gestion Renault"
+    if(X==13): return "Japon"
+    if(X==14): return "Manager"
+    if(X==15): return "Mécanicien"
+    if(X==16): return "Médical"
+    if(X==17): return "Nuit"
+    if(X==18): return "Prestataires"
+    if(X==19): return "Regulation Medicale"
+    if(X==20): return "RENAULT"
+    if(X==21): return "RTC"
+    if(X==22): return "SAP"
+    if(X==23): return "Services"
+    if(X==24): return "Tech. Axa"
+    if(X==25): return "Tech. Inter"
+    if(X==26): return "Tech. Total"
+    if(X==27): return "Téléphonie"
     return -1 #autre cas randoms si ça foire
 
 def holidays(X):
